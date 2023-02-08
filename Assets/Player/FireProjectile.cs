80a3ec0ac30cd3494b54e964f51fff103262f174
PhoenixFire <72724975+help-programming-is-hard@users.noreply.github.com>
2023-02-08 09:52:58 -0800

    fireManager completed

--- Assets/Player/FireProjectile.cs
+++ Assets/Player/FireProjectile.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class FireProjectile : MonoBehaviour
 {
     [SerializeField] private Transform firePoint;
+    [SerializeField] private Vector3 recoilVector;
+    [SerializeField] private Rigidbody rb;
 
     [SerializeField] private AudioClip shootSound;
     [SerializeField] private AudioSource audioSource;
@@ -13,10 +15,19 @@ public class FireProjectile : MonoBehaviour
 
     [SerializeField] private float shootInput;
     [SerializeField] private float shootDelay;
+    [SerializeField] private float timeSinceLastShot;
+
+
+    [SerializeField] private bool _debug;
 
     // Start is called before the first frame update
     void Start()
     {
+        if (rb == null) { rb = gameObject.GetComponent<Rigidbody>();}
+        if (rb == null)
+        {
+            Debug.LogError("FireProjectile: Rigidbody is null");
+        }
         if (audioSource == null) {audioSource = transform.GetChild(0).transform.GetChild(0).GetComponent<AudioSource>();}
         if (audioSource == null)
         {
@@ -40,13 +51,26 @@ public class FireProjectile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        shootDelay += Time.deltaTime;    
+        timeSinceLastShot += Time.deltaTime;    
         shootInput = Input.GetAxis("Fire1");
 
-        if (shootInput >= shootDelay)
+        if (shootInput != 0 && timeSinceLastShot >= shootDelay)
         {
-            shootDelay = 0;
+            timeSinceLastShot = 0;
             Instantiate(projectile, firePoint.position, firePoint.rotation);
+            rb.AddRelativeForce(recoilVector, ForceMode.Impulse);
+
         }
     }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        if (_debug)
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawCube(firePoint.position, new Vector3(0.1f, 0.1f, 0.1f));
+        }
+    }
+#endif
 }
